2d10337634b7224c0a8cfe98da6dfcb798c3d3a6
japaakihiro <larahbrenda@hotmail.com>
2023-07-01 11:32:14 -0300

    fix: tailwindcss sytax

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,5 +1,4 @@
 import * as Form from "@radix-ui/react-form";
-import background from "../../assets/background.png";
 import "./styles.css";
 import * as Checkbox from "@radix-ui/react-checkbox";
 
@@ -9,18 +8,18 @@ const RegisterPage = () => {
             <header className="title">Criar uma conta</header>
             <h3>Seus dados</h3>
             <Form.Root>
-                <Form.Field className="grid mb-[10px]" name="profile_photo">
+                <Form.Field className="mb-[10px] grid" name="profile_photo">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white"></Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome hovr:shadow-[0_0_0_1px_black] selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 focus:shadow-[0_0_0_2px_black]"
                             type="text"
                             required
                         />
                     </Form.Control>
                     <Form.Message className="text-[13px] text-white opacity-[0.8]" match="typeMismatch"></Form.Message>
                 </Form.Field>
-                <Form.Field className="grid mb -[10px]" name="full_name">
+                <Form.Field className="mb -[10px] grid" name="full_name">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="text-[15px] font-medium leading-[35px] text-white">Email</Form.Label>
                         <Form.Message className="text-[13px] text-white opacity-[0.8]" match="valueMissing">
@@ -30,105 +29,105 @@ const RegisterPage = () => {
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">Nome Completo</Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="text"
                             required
                         />
                     </Form.Control>
                 </Form.Field>
 
-                <Form.Field className="grid mb-[10px]" name="birth_date">
+                <Form.Field className="mb-[10px] grid" name="birth_date">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
                         Data de Nascimento
                     </Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="date"
                             required
                         />
                     </Form.Control>
                 </Form.Field>
 
-                <Form.Field className="grid mb-[10px]" name="email">
+                <Form.Field className="mb-[10px] grid" name="email">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">Email</Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="email"
                             required
                         />
                     </Form.Control>
                 </Form.Field>
 
-                <Form.Field className="grid mb-[10px]" name="email_confirm">
+                <Form.Field className="mb-[10px] grid" name="email_confirm">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
                         Confirmar Email
                     </Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="email"
                             required
                         />
                     </Form.Control>
                 </Form.Field>
 
-                <Form.Field className="grid mb-[10px]" name="work_institute">
+                <Form.Field className="mb-[10px] grid" name="work_institute">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
                         Instituição de Trabalho
                     </Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="text"
                             required
                         />
                     </Form.Control>
                 </Form.Field>
 
-                <Form.Field className="grid mb-[10px]" name="state_country">
+                <Form.Field className="mb-[10px] grid" name="state_country">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
                         Estado de Atuação
                     </Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="text"
                             required
                         />
                     </Form.Control>
                 </Form.Field>
 
-                <Form.Field className="grid mb-[10px]" name="phone_number">
+                <Form.Field className="mb-[10px] grid" name="phone_number">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">Telephone</Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="text"
                             required
                         />
                     </Form.Control>
                 </Form.Field>
 
-                <Form.Field className="grid mb-[10px]" name="password">
+                <Form.Field className="mb-[10px] grid" name="password">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">Senha</Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="password"
                             required
                         />
                     </Form.Control>
                 </Form.Field>
 
-                <Form.Field className="grid mb-[10px]" name="password_confirm">
+                <Form.Field className="mb-[10px] grid" name="password_confirm">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
                         Confirmar Senha
                     </Form.Label>
                     <Form.Control asChild>
                         <input
-                            className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex h-[35px] appearence=none items-center justify-center rounded-[4px] px-[10px] text-[15px] leadind-none text-white shadow-[0_0_0_1px] outline-nome hovr: shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection: color-white selection: bg-blackA9"
+                            className="appearence=none leadind-none outline-nome selection: color-white box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blackA5 px-[10px] text-[15px] text-white shadow-[0_0_0_1px] shadow-blackA9 selection:bg-blackA9 hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                             type="password"
                             required
                         />
