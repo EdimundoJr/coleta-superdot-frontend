4bf857c68eb8057ea4173816bb6a075e3d26ddcf
DVastMasters <contatorafaeldutra@outlook.com>
2023-07-04 16:25:27 -0300

    refactor: using env variables to store constants

--- src/utils/tokensHandler.ts
+++ src/utils/tokensHandler.ts
@@ -1,22 +1,35 @@
 import axios from "axios";
 
-export const saveTokens = (accessToken: string, refreshToken: string) => {
-    localStorage.setItem("accessToken", accessToken);
-    localStorage.setItem("refreshToken", refreshToken);
+export interface Tokens {
+    accessToken: string;
+    refreshToken: string;
+}
+
+export const saveTokens = ({ accessToken, refreshToken }: Tokens) => {
+    localStorage.setItem(import.meta.env.VITE_ACCESS_TOKEN_KEY, accessToken);
+    localStorage.setItem(import.meta.env.VITE_REFRESH_TOKEN_KEY, refreshToken);
     setAuthHeaders();
 };
 
 export const clearTokens = () => {
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
+    localStorage.removeItem(import.meta.env.VITE_ACCESS_TOKEN_KEY);
+    localStorage.removeItem(import.meta.env.VITE_REFRESH_TOKEN_KEY);
     setAuthHeaders();
 };
 
 export const setAuthHeaders = () => {
     axios.interceptors.request.use((request) => {
-        request.headers.set("Authorization", `Bearer ${localStorage.getItem("accessToken")}`);
-        request.headers.set("X-Refresh", localStorage.getItem("refreshToken"));
+        request.headers.set("Authorization", `Bearer ${localStorage.getItem(import.meta.env.VITE_ACCESS_TOKEN_KEY)}`);
+        request.headers.set("X-Refresh", localStorage.getItem(import.meta.env.VITE_REFRESH_TOKEN_KEY));
 
         return request;
     });
+};
+
+export const isLoggedIn = (): boolean => {
+    const accessToken = localStorage.getItem(import.meta.env.VITE_ACCESS_TOKEN_KEY);
+    if (accessToken) {
+        return true;
+    }
+    return false;
 };
