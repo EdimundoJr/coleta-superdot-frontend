ed376ea6daeb0fffbce65c273ff504935322a81f
DVastMasters <contatorafaeldutra@outlook.com>
2023-07-06 16:08:32 -0300

    new: Modal component

---
+++ src/components/Inner/Modal/Modal.tsx
@@ -0,0 +1,39 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import * as Separator from "@radix-ui/react-separator";
+
+interface ModalProps extends React.PropsWithChildren {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+    title: string;
+    accessibleDescription: string;
+}
+
+const Modal = ({ title, accessibleDescription, open, setOpen, children }: ModalProps) => {
+    return (
+        <Dialog.Root open={open} onOpenChange={setOpen}>
+            <Dialog.Portal>
+                <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-blackA9" />
+                <Dialog.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[100vh] w-[90vw] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] text-black  shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none sm:max-w-[60vw]">
+                    <Dialog.Title>{title}</Dialog.Title>
+                    <Separator.Root className="my-5 h-px w-full bg-gray-300" />
+                    <VisuallyHidden.Root asChild>
+                        <Dialog.Description>{accessibleDescription}</Dialog.Description>
+                    </VisuallyHidden.Root>
+                    {children}
+                    <Dialog.Close asChild>
+                        <button
+                            className="absolute right-[10px] top-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full text-violet11 hover:bg-violet4 focus:shadow-[0_0_0_2px] focus:shadow-violet7 focus:outline-none"
+                            aria-label="Close"
+                        >
+                            <Cross2Icon />
+                        </button>
+                    </Dialog.Close>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    );
+};
+
+export default Modal;
