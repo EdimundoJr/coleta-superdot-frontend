ca046925c9328ecd50e91850234c6c47354c7b6d
DVastMasters <contatorafaeldutra@outlook.com>
2023-07-06 16:13:57 -0300

    refactor: adding menu toggle to NavBar

--- src/components/Inner/InnerNavBar/InnerNavBar.tsx
+++ src/components/Inner/InnerNavBar/InnerNavBar.tsx
@@ -1,20 +1,37 @@
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
-import { ExitIcon, PersonIcon } from "@radix-ui/react-icons";
+import { ExitIcon, HamburgerMenuIcon, PersonIcon } from "@radix-ui/react-icons";
 import { logoutUser } from "../../../api/auth.api";
 import { Link } from "react-router-dom";
 
-const InnerNavBar = () => {
+interface InnerNavBarProps {
+    onExpandMenuClicked: () => void;
+    isMobile: boolean;
+}
+
+const InnerNavBar = ({ onExpandMenuClicked, isMobile }: InnerNavBarProps) => {
     return (
         <NavigationMenu.Root className="bg-white drop-shadow-xl">
-            <NavigationMenu.List>
-                <NavigationMenu.Item className="flex justify-end p-4 text-black">
-                    <Link to="home" className="mx-3">
-                        <PersonIcon className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px]" />
-                    </Link>
-                    <Link onClick={logoutUser} to="/" className="mx-3">
-                        <ExitIcon className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px]" />
-                    </Link>
-                </NavigationMenu.Item>
+            <NavigationMenu.List className={`flex ${!isMobile ? "justify-end" : "justify-between"} p-4`}>
+                {isMobile && (
+                    <NavigationMenu.Item className="flex items-center text-black">
+                        <HamburgerMenuIcon
+                            onClick={onExpandMenuClicked}
+                            className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px]"
+                        />
+                    </NavigationMenu.Item>
+                )}
+                <div className="flex">
+                    <NavigationMenu.Item className="mx-3 flex items-center text-black">
+                        <Link to="home">
+                            <PersonIcon className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px]" />
+                        </Link>
+                    </NavigationMenu.Item>
+                    <NavigationMenu.Item className="mx-3 flex items-center text-black">
+                        <Link onClick={logoutUser} to="/">
+                            <ExitIcon className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px]" />
+                        </Link>
+                    </NavigationMenu.Item>
+                </div>
             </NavigationMenu.List>
         </NavigationMenu.Root>
     );
