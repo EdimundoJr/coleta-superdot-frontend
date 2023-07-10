7b8515d2c5b475968cb520f5fcaf8e4c3f4fb831
DVastMasters <contatorafaeldutra@outlook.com>
2023-07-10 16:39:50 -0300

    new: api functions to Sample and SampleReview

--- src/api/sample.api.ts
+++ src/api/sample.api.ts
@@ -1,17 +1,16 @@
 import axios from "axios";
 import { setAuthHeaders } from "../utils/tokensHandler";
 import { SampleValues } from "../schemas/sample.schema";
+import { SampleStatus } from "../utils/consts.utils";
 
 export const FILES_TO_UPLOAD = [
     {
         key: "research_cep[research_document]",
         label: "Projeto de pesquisa",
-        required: true,
     },
     {
         key: "research_cep[tcle_document]",
         label: "TCLE",
-        required: true,
     },
     {
         key: "research_cep[tale_document]",
@@ -22,4 +21,44 @@ export const FILES_TO_UPLOAD = [
 export const createSample = async (sampleData: FormData) => {
     setAuthHeaders();
     return axios.post<SampleValues>(`${import.meta.env.VITE_BACKEND_HOST}/api/sample/newSample`, sampleData);
+};
+
+export interface SampleSummary {
+    researcher_id: string;
+    sample_id: string;
+    sample_name: string;
+    researcher_name: string;
+    cep_code: string;
+    qtt_participants_requested: number;
+    qtt_participants_authorized?: number;
+    currentStatus: SampleStatus;
+    files: {
+        research_document: string;
+        tcle_document: string;
+        tale_document: string;
+    };
+}
+
+export interface Page {
+    pagination: {
+        totalItems: number;
+        page: 1;
+    };
+    data: [SampleSummary];
+}
+
+export const paginateAllSamples = async (currentPage: number, itemsPerPage: number, filterStatus = "") => {
+    setAuthHeaders();
+    return axios.get<Page>(
+        `${
+            import.meta.env.VITE_BACKEND_HOST
+        }/api/sample/paginateAll/${itemsPerPage}/page/${currentPage}?status=${filterStatus}`
+    );
+};
+
+export const seeAttachment = async (fileName: string) => {
+    setAuthHeaders();
+    return axios.get(`${import.meta.env.VITE_BACKEND_HOST}/api/sample/attachment/${fileName}`, {
+        responseType: "blob",
+    });
 };
---
+++ src/api/sampleReview.api.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { setAuthHeaders } from "../utils/tokensHandler";
+import { SampleStatus } from "../utils/consts.utils";
+
+interface ReviewValues {
+    sample_id: string;
+    next_status: SampleStatus;
+    qtt_participants_authorized?: number;
+    review_message: string;
+}
+
+export const createReview = async (reviewData: ReviewValues) => {
+    setAuthHeaders();
+    return axios.post(`${import.meta.env.VITE_BACKEND_HOST}/api/sampleReview/newReview`, reviewData);
+};
+
+interface ISampleReview {
+    _id?: string;
+    previous_status?: SampleStatus;
+    next_status: SampleStatus;
+    qtt_participants_authorized?: number;
+    review_message: string;
+    reviewer_id: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface SampleReviewWithReviewerName {
+    review_details: ISampleReview;
+    reviewer_full_name: string;
+}
+
+export const findReviewsBySampleId = async (sampleId: string) => {
+    setAuthHeaders();
+    return axios.get<SampleReviewWithReviewerName[]>(
+        `${import.meta.env.VITE_BACKEND_HOST}/api/sampleReview/findReviews/${sampleId}`
+    );
+};
