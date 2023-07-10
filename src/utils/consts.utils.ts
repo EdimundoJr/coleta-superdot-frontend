d27e5519a0557949df7151fa14385e6dabc4099c
DVastMasters <contatorafaeldutra@outlook.com>
2023-07-10 16:39:00 -0300

    new: util file to define constants

---
+++ src/utils/consts.utils.ts
@@ -0,0 +1,7 @@
+export const SAMPLE_STATUS_ARRAY = ["Pendente", "Autorizado", "Não Autorizado"] as const;
+export type SampleStatus = "Pendente" | "Autorizado" | "Não Autorizado";
+
+export const INSTITUITION_TYPE_ARRAY = ["Pública", "Particular"] as const;
+export type InstituitionType = "Pública" | "Particular";
+
+export const BRAZIL_REGIONS_ARRAY = ["Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"] as const;
