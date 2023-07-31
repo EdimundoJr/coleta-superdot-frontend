1430453b8e6937533e3434531d6d17fe0c529415
DVastMasters <contatorafaeldutra@outlook.com>
2023-07-31 08:28:19 -0300

    refactor: defined a max height to card component

--- src/components/Card/CardHeader.tsx
+++ src/components/Card/CardHeader.tsx
@@ -1,5 +1,5 @@
 const CardHeader = ({ children }: React.PropsWithChildren) => {
-    return <div className="flex justify-center gap-1 px-2 py-4">{children}</div>;
+    return <div className="flex justify-center gap-1 px-2 py-4 text-lg">{children}</div>;
 };
 
 export default CardHeader;
