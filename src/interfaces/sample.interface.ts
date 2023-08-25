36244b2a62dca2e3bb8baf58133a0f1ae9e3d05e
DVastMasters <contatorafaeldutra@outlook.com>
2023-08-25 09:48:18 -0300

    feat: created an interface to accept sample files

--- src/interfaces/sample.interface.ts
+++ src/interfaces/sample.interface.ts
@@ -1,8 +1,15 @@
 export interface SampleFile {
     key: string; // FormData field
-    jsonFileKey?: string // JSON backend field
-    backendFileName?: string
+    jsonFileKey?: string; // JSON backend field
+    backendFileName?: string;
     label: string; // Display name to user
     required?: boolean;
     uploadedFile?: File;
+}
+
+export interface AcceptSampleFile {
+    jsonFileKey: string;
+    backendFileName: string;
+    label: string;
+    accepted?: boolean;
 }
