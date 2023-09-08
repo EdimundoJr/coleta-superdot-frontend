dcb061a33eadf725ac5d3683e9fe2c84d7fa8eaa
DVastMasters <contatorafaeldutra@outlook.com>
2023-09-08 11:35:54 -0300

    refactor: cleaning participant registration table

--- src/components/Table/ParticipantsRegistrationTable/ParticipantsRegistrationTable.tsx
+++ src/components/Table/ParticipantsRegistrationTable/ParticipantsRegistrationTable.tsx
@@ -1,63 +1,97 @@
 import { CopyIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import { Page, PageSampleSummary, ParticipantFormSummary } from "../../../api/sample.api";
 import { DateTime } from "luxon";
+import { ISample } from "../../../interfaces/sample.interface";
+import { TParticipantFormProgress } from "../../../utils/consts.utils";
+import { EAdultFormSteps } from "../../../pages/AdultForm/AdultForm";
+import { ISecondSource } from "../../../interfaces/secondSource.interface";
+import { IParticipant } from "../../../interfaces/participant.interface";
 
 interface ParticipantsRegistrationTableProps {
-    page?: Page<ParticipantFormSummary>;
+    sampleId: string;
+    data?: ISample["participants"];
     currentPage: number;
     setCurrentPage: (newPage: number) => void;
-    onClickToViewSecondSources: (participantId: string) => void;
+    onClickToViewSecondSources: (participant: IParticipant) => void;
+    onClickToCopySecondSourceURL: (text: string) => void;
 }
 
 const ParticipantsRegistrationTable = ({
-    page,
+    sampleId,
+    data,
     currentPage,
     setCurrentPage,
     onClickToViewSecondSources,
+    onClickToCopySecondSourceURL,
 }: ParticipantsRegistrationTableProps) => {
+    const getParticipantProgress = (
+        currentStep: EAdultFormSteps | undefined,
+        secondSources: ISecondSource[] | undefined
+    ): TParticipantFormProgress => {
+        if (currentStep === EAdultFormSteps.FINISHED) {
+            if (secondSources?.length) {
+                const allSecondSourcesFillingForm = secondSources?.every(
+                    (secSource) => secSource.adultFormCurrentStep !== EAdultFormSteps.FINISHED
+                );
+                if (allSecondSourcesFillingForm) return "Aguardando 2ª fonte";
+                else return "Finalizado";
+            }
+        }
+        return "Preenchendo";
+    };
+
     return (
         <table className="bg-dark-gradient mx-auto w-11/12 border-collapse rounded-md text-alternative-text">
             <thead>
                 <tr>
-                    <th className=" px-6 py-3">Nome</th>
-                    <th className=" px-6 py-3">Andamento</th>
-                    <th className=" px-6 py-3">2ªs Fontes</th>
-                    <th className=" px-6 py-3">Data de início</th>
-                    <th className=" px-6 py-3">Data de fim</th>
-                    <th className=" px-6 py-3">Indicadores de AH/SD</th>
-                    <th className=" px-6 py-3">URL 2ª fonte</th>
+                    <th>Nome</th>
+                    <th>Andamento</th>
+                    <th>2ªs Fontes</th>
+                    <th>Data de início</th>
+                    <th>Data de fim</th>
+                    <th>Indicadores de AH/SD</th>
+                    <th>URL 2ª fonte</th>
                 </tr>
             </thead>
             <tbody className="bg-white text-primary">
-                {page?.data?.map((participant) => (
+                {data?.map((participant) => (
                     <tr key={participant._id} className="odd:bg-gray-200">
-                        <td className="border-x-2 border-primary px-6 py-3">{participant.fullName}</td>
-                        <td className="border-x-2 border-primary px-6 py-3">{participant.formFillStatus}</td>
-                        <td className="border-x-2 border-primary px-6 py-3 text-center">
-                            <div className="flex justify-center">
-                                {participant.qttSecondSources}
-                                <MagnifyingGlassIcon className="cursor-pointer" />
+                        <td>{participant.personalData.fullName}</td>
+                        <td>{getParticipantProgress(participant.adultFormCurrentStep, participant.secondSources)}</td>
+                        <td>
+                            <div className="flex items-center justify-center">
+                                {participant.secondSources?.length}
+                                <MagnifyingGlassIcon
+                                    className="cursor-pointer"
+                                    onClick={() => onClickToViewSecondSources(participant)}
+                                />
                             </div>
                         </td>
-                        <td className="border-x-2 border-primary px-6 py-3">
-                            {DateTime.fromISO(participant.formFillStartDate || "").toFormat("dd/LL/yyyy - HH:mm")}
+                        <td>{DateTime.fromISO(participant.createdAt).toFormat("dd/LL/yyyy - HH:mm")}</td>
+                        <td>
+                            {participant.endFillFormDate &&
+                                DateTime.fromISO(participant.endFillFormDate).toFormat("dd/LL/yyyy - HH:mm")}
                         </td>
-                        <td className="border-x-2 border-primary px-6 py-3">
-                            {participant.formFillEndDate &&
-                                DateTime.fromISO(participant.formFillEndDate || "").toFormat("dd/LL/yyyy - HH:mm")}
-                        </td>
-                        <td className="border-x-2 border-primary px-6 py-3">
-                            {participant.giftednessIndicators ? "Sim" : "Não"}
-                        </td>
-                        <td className="border-x-2 border-primary px-6 py-3">
-                            <CopyIcon />
+                        <td>{participant.giftdnessIndicators ? "Sim" : "Não"}</td>
+                        <td>
+                            <div className="flex justify-center">
+                                <CopyIcon
+                                    className="cursor-pointer"
+                                    onClick={() =>
+                                        onClickToCopySecondSourceURL(
+                                            `${
+                                                import.meta.env.VITE_FRONTEND_URL
+                                            }/formulario-adulto-segunda-fonte/${sampleId}/${participant._id}`
+                                        )
+                                    }
+                                />
+                            </div>
                         </td>
                     </tr>
                 ))}
             </tbody>
             <tfoot>
                 <tr className="text-right">
-                    <td className="px-6 py-3" colSpan={3}></td>
+                    <td colSpan={3}></td>
                 </tr>
             </tfoot>
         </table>
