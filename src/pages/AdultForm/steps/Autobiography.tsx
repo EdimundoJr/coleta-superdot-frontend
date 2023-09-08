21f01b3554884c26853911f5e41261b44e3a813e
DVastMasters <contatorafaeldutra@outlook.com>
2023-09-08 11:30:06 -0300

    feat: created the last adult form step page

---
+++ src/pages/AdultForm/steps/Autobiography.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { submitAutobiography } from "../../../api/participant.api";
+
+interface AutobiographyStepProps {
+    nextStep: () => void;
+    setNotificationTitle: (title: string) => void;
+    setNotificationDescription: (description: string) => void;
+    sampleId: string;
+}
+
+const AutobiographyStep = ({
+    nextStep,
+    setNotificationTitle,
+    setNotificationDescription,
+    sampleId,
+}: AutobiographyStepProps) => {
+    const [autobiographyText, setAutobiographyText] = useState("");
+    const [autobiographyVideo, setAutobiographyVideo] = useState("");
+
+    const handleSubmit = async () => {
+        if (!autobiographyText && !autobiographyVideo) {
+            setNotificationTitle("Preencha pelo menos um campo");
+            setNotificationDescription("É obrigatório digitar/gravar a autobiografia.");
+            return;
+        }
+
+        try {
+            const response = await submitAutobiography({ sampleId, autobiographyText, autobiographyVideo });
+            if (response.status === 200) {
+                setNotificationTitle("Questionário finalizado!");
+                setNotificationDescription("Agradecemos pelas respostas. Em breve o pesquisador entrará em contato.");
+                nextStep();
+            }
+        } catch (e) {
+            setNotificationTitle("Erro no servidor!");
+            setNotificationDescription(
+                "Não foi possível enviar a sua autobiografia. Salve o conteúdo que foi escrito e contate os responsáveis pelo sistema."
+            );
+            console.error(e);
+        }
+    };
+
+    return (
+        <div className="grid gap-y-5">
+            <header className="my-6">
+                <h1>Autobiografia</h1>
+                <h3>
+                    Conte um pouco sobre você. Você pode escrever sua própria biografia ou, caso prefira, pode gravar um
+                    vídeo falando sobre si.
+                </h3>
+            </header>
+
+            <label htmlFor="autobiographyText">ESCREVA SOBRE VOCÊ</label>
+            <textarea
+                onChange={(e) => setAutobiographyText(e.target.value)}
+                rows={10}
+                id="autobiographyText"
+            ></textarea>
+            <label htmlFor="autobiographyVideo">
+                COLE A URL DO SEU VÍDEO DO YOUTUBE NO CAMPO ABAIXO (NÃO DEIXE O VÍDEO PRIVADO)
+            </label>
+            <input onChange={(e) => setAutobiographyVideo(e.target.value)} id="autobiographyVideo"></input>
+
+            <button onClick={handleSubmit} className="button-secondary mx-auto w-1/3">
+                Continuar
+            </button>
+        </div>
+    );
+};
+
+export default AutobiographyStep;
