3d0c283710ce1568e188d9c0a56fcf2154109c61
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-02 09:24:10 -0300

    feat: improve the emphasis in current step

--- src/components/Stepper/StepperStep.tsx
+++ src/components/Stepper/StepperStep.tsx
@@ -15,21 +15,21 @@ const StepperStep = ({ stepNumber, stepTitle, stepDescription, stepState }: Step
             case "DONE":
                 return "bg-primary border-primary";
             case "HOLD":
-                return "border-light-primary";
+                return "border-light-primary bg-primary";
             case "DISABLED":
                 return "border-gray-500";
         }
     }
 
     return (
-        <div className="flex items-center gap-2">
+        <div className={`flex items-center gap-2 ${stepState === "HOLD" && "border-[3px] border-primary px-5 py-2"}`}>
             <div className={`h-[20px] w-[20px] border-[3px] ${getVectorClasses()} rounded-full`}>
-                {stepState === "DONE" && <CheckIcon className="text-white" />}
+                {stepState === "DONE" && <CheckIcon className="text-gray-300" />}
             </div>
-            <h4 className={`${stepState === "DISABLED" && "text-gray-300"}`}>{stepNumber}</h4>
+            <h4 className={`${stepState !== "HOLD" && "text-gray-300"}`}>{stepNumber}</h4>
             <div>
-                <div className={`subtitle-2 ${stepState === "DISABLED" && "text-gray-300"}`}>{stepTitle}</div>
-                <div className={`caption ${stepState === "DISABLED" && "text-gray-300"}`}>{stepDescription}</div>
+                <div className={`subtitle-2 ${stepState !== "HOLD" && "text-gray-300"}`}>{stepTitle}</div>
+                <div className={`caption ${stepState !== "HOLD" && "text-gray-300"}`}>{stepDescription}</div>
             </div>
         </div>
     );
