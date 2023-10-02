dbb2d30b0be7902f753930d8b814831ddcd6c7de
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-02 09:50:56 -0300

    feat: props to remove background from stepper

--- src/components/Stepper/StepperRoot.tsx
+++ src/components/Stepper/StepperRoot.tsx
@@ -2,10 +2,15 @@ import StepperStep from "./StepperStep";
 
 interface StepperRootProps {
     children: React.ReactElement<typeof StepperStep>[] | React.ReactElement<typeof StepperStep>;
+    removeBackground?: boolean;
 }
 
-const StepperRoot = ({ children }: StepperRootProps) => {
-    return <div className="m-4 justify-between bg-violet-400 p-4 px-10 sm:flex">{children}</div>;
+const StepperRoot = ({ children, removeBackground }: StepperRootProps) => {
+    return (
+        <div className={`m-4 justify-between ${!removeBackground && "bg-violet-400"} p-4 px-10 sm:flex`}>
+            {children}
+        </div>
+    );
 };
 
 export default StepperRoot;
