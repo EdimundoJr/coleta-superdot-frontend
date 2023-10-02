8c981b067ac97a9e7251ed6f3d355f65297d2331
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-02 09:54:14 -0300

    refactor: cleaning stepper step tailwind classes

--- src/components/Stepper/StepperStep.tsx
+++ src/components/Stepper/StepperStep.tsx
@@ -7,29 +7,39 @@ interface StepperStepProps {
     stepTitle: string;
     stepDescription: string;
     stepState: StepStateType;
+    whiteContrast?: boolean;
 }
 
-const StepperStep = ({ stepNumber, stepTitle, stepDescription, stepState }: StepperStepProps) => {
-    function getVectorClasses() {
-        switch (stepState) {
-            case "DONE":
-                return "bg-primary border-primary";
-            case "HOLD":
-                return "border-light-primary bg-primary";
-            case "DISABLED":
-                return "border-gray-500";
-        }
+const StepperStep = ({ stepNumber, stepTitle, stepDescription, stepState, whiteContrast }: StepperStepProps) => {
+    let backgroundClasses = "flex items-center gap-2";
+    if (stepState === "HOLD") {
+        backgroundClasses += " border-[1px] border-primary px-5 py-2";
+    }
+
+    let circleClasses = "h-[20px] w-[20px] border-[3px] rounded-full ";
+    switch (stepState) {
+        case "DONE":
+            circleClasses += "bg-primary border-primary";
+            break;
+        case "HOLD":
+            circleClasses += "border-light-primary bg-primary";
+            break;
+        case "DISABLED":
+            circleClasses += "border-gray-500";
+    }
+
+    let textClasses = stepState !== "HOLD" ? "text-gray-300" : "";
+    if (whiteContrast) {
+        textClasses = "text-gray-700";
     }
 
     return (
-        <div className={`flex items-center gap-2 ${stepState === "HOLD" && "border-[3px] border-primary px-5 py-2"}`}>
-            <div className={`h-[20px] w-[20px] border-[3px] ${getVectorClasses()} rounded-full`}>
-                {stepState === "DONE" && <CheckIcon className="text-gray-300" />}
-            </div>
-            <h4 className={`${stepState !== "HOLD" && "text-gray-300"}`}>{stepNumber}</h4>
+        <div className={backgroundClasses}>
+            <div className={circleClasses}>{stepState === "DONE" && <CheckIcon className="text-gray-300" />}</div>
+            <h4 className={textClasses}>{stepNumber}</h4>
             <div>
-                <div className={`subtitle-2 ${stepState !== "HOLD" && "text-gray-300"}`}>{stepTitle}</div>
-                <div className={`caption ${stepState !== "HOLD" && "text-gray-300"}`}>{stepDescription}</div>
+                <div className={`subtitle-2 ${textClasses}`}>{stepTitle}</div>
+                <div className={`caption ${textClasses}`}>{stepDescription}</div>
             </div>
         </div>
     );
