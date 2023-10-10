2e204ce54d4340d4b44412ec12e6ae5161cb8b4c
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-10 13:36:01 -0300

    feat: created a four select component

---
+++ src/components/FourSelect/FourSelect.tsx
@@ -0,0 +1,46 @@
+interface FourSelectProps {
+    options: string[];
+    values: string[] | undefined;
+    onChange: (newValues: string[]) => void;
+}
+
+const FourSelect = ({ options, values, onChange }: FourSelectProps) => {
+    const getCurrentSelectValue = (idx: number) => {
+        return values ? values[idx] : "";
+    };
+
+    const changeCurrentSelectValue = (idx: number, newValue: string) => {
+        if (values !== undefined) {
+            onChange(values.map((value, i) => (i === idx ? newValue : value)));
+            return;
+        }
+
+        const arrValues = Array(4);
+        arrValues[idx] = newValue;
+        onChange(arrValues);
+    };
+
+    return (
+        <div className="mx-auto mt-4 grid grid-cols-1 justify-center gap-5 sm:w-1/3">
+            {/* CREATE AN ITERATOR FROM 0 TO 3 */}
+            {[...Array(4).keys()].map((idx) => (
+                <div className="flex items-center gap-5" key={idx}>
+                    <label>{idx + 1}º</label>
+                    <select
+                        placeholder="Digite aqui"
+                        value={getCurrentSelectValue(idx)}
+                        onChange={(e) => {
+                            changeCurrentSelectValue(idx, e.target.value);
+                        }}
+                    >
+                        {options.map((option) => (
+                            <option>{option}</option>
+                        ))}
+                    </select>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default FourSelect;
