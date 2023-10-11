e8aa855f76c20223499414d2c502b5bf7a79354e
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-11 10:38:34 -0300

    feat: render quest based on question parent answer

--- src/components/RenderQuestions/RenderQuestions.tsx
+++ src/components/RenderQuestions/RenderQuestions.tsx
@@ -14,7 +14,6 @@ interface RenderQuestionsProps {
  * Component to render all types of questions
  */
 const RenderQuestions = ({ questions, setQuestions }: RenderQuestionsProps) => {
-    console.log(questions);
     const answerQuestionWithWithoutArray = (questionId: string, answer: string) => {
         setQuestions(
             questions.map((question) => {
@@ -36,6 +35,15 @@ const RenderQuestions = ({ questions, setQuestions }: RenderQuestionsProps) => {
         }
     };
 
+    const renderChildQuestion = (childQuestion: IQuestion) => {
+        const parentQuestion = questions.find((question) => question._id === childQuestion.parentQuestion?.parentId);
+        if (parentQuestion?.answer?.includes(childQuestion.parentQuestion?.isRequiredOnParentValue || "")) {
+            return true;
+        }
+
+        return false;
+    };
+
     return (
         <>
             {questions?.map((question) => (
@@ -51,27 +59,32 @@ const RenderQuestions = ({ questions, setQuestions }: RenderQuestionsProps) => {
 
                     {question.questionType === EQuestionType.MULTIPLE_SELECT && (
                         <MultipleSelect
-                            options={question.options as string[]}
+                            options={question.options?.map((option) => option.value) as string[]}
                             placeholder="Caso se destaque, selecione uma ou várias opções"
                             values={question.answer as string[]}
                             onChange={(values) => answerQuestionWithArray(question._id, values)}
                         />
                     )}
 
-                    {question.questionType === EQuestionType.ONE_INPUT && (
-                        <div className="mx-auto mt-4 grid grid-cols-1 justify-center gap-5 sm:w-1/4">
-                            <input
-                                key={question._id}
-                                placeholder="Digite aqui"
-                                value={question.answer}
-                                onChange={(e) => answerQuestionWithWithoutArray(question._id, e.target.value)}
-                            />
-                        </div>
-                    )}
+                    {/* This code block is rendering an input field if the question type is ONE_INPUT
+                    and it has a parent question. It checks if the child question should be rendered
+                    based on the answer of the parent question. */}
+                    {question.questionType === EQuestionType.ONE_INPUT &&
+                        question.parentQuestion &&
+                        renderChildQuestion(question) && (
+                            <div className="mx-auto mt-4 grid grid-cols-1 justify-center gap-5 sm:w-1/4">
+                                <input
+                                    key={question._id}
+                                    placeholder="Digite aqui"
+                                    value={question.answer}
+                                    onChange={(e) => answerQuestionWithWithoutArray(question._id, e.target.value)}
+                                />
+                            </div>
+                        )}
 
                     {question.questionType === EQuestionType.FIVE_OPTION && (
                         <FiveOption
-                            options={question.options as string[]}
+                            options={question.options?.map((option) => option.value) as string[]}
                             value={question.answer as string}
                             onSelect={(v) => answerQuestionWithWithoutArray(question._id, v)}
                         />
@@ -79,7 +92,7 @@ const RenderQuestions = ({ questions, setQuestions }: RenderQuestionsProps) => {
 
                     {question.questionType === EQuestionType.FOUR_SELECT && (
                         <FourSelect
-                            options={question.options as string[]}
+                            options={question.options?.map((option) => option.value) as string[]}
                             values={question.answer as string[]}
                             onChange={(v) => answerQuestionWithArray(question._id, v)}
                         />
